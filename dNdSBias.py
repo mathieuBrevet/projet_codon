083fa9eab71ac265d058158fcf95713a6036fc74
ThibaultLatrille <thibault.latrille@ens-lyon.org>
2018-01-15 10:49:41 +0100

    No computation of null-spaces, not necessary anymore

--- dNdSBias.py
+++ dNdSBias.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.linalg import expm
 import matplotlib.pyplot as plt
 
 RED = "#EB6231"
@@ -7,34 +6,6 @@ YELLOW = "#E29D26"
 BLUE = "#5D80B4"
 LIGHTGREEN = "#6ABD9B"
 GREEN = "#8FB03E"
-
-
-def abs_sum(array):
-    return np.sum(np.abs(array))
-
-
-def nullspace(A, atol=1e-13, rtol=0):
-    A = np.atleast_2d(A)
-    u, s, vh = np.linalg.svd(A)
-    tol = max(atol, rtol * s[0])
-    nnz = (s >= tol).sum()
-    ns = vh[nnz:].conj().T
-    return ns
-
-
-def equilibrium_frequencies(matrix):
-    null_vec = nullspace(matrix.T)
-    if null_vec.shape[0] != 61 and null_vec.shape[1] != 1:
-        "The null space is not of dimension 1"
-        return null_vec
-    else:
-        frequencies = null_vec.T[0]
-        frequencies /= np.sum(frequencies)
-        assert abs(np.sum(frequencies) - 1) < 1e-10, "The frequencies does not sum to 1"
-        assert abs_sum(np.dot(frequencies, matrix)) < 1e-10, "The frequencies are not the nullspace of the transposed matrix"
-        assert abs_sum(np.dot(frequencies, expm(matrix)) - frequencies) < 1e-10, "The frequencies are not stationary"
-        return frequencies
-
 
 nucleotides = "ACGT"
 codontable = {
@@ -69,7 +40,7 @@ z_ss = 3 - nbr_weak
 amino_acids_set = set(codontable.values())
 amino_acids_set.remove('X')
 amino_acids = "".join(amino_acids_set) + "X"
-assert len(amino_acids) == 21, "There is not 20 amino-acids in the codon table"
+assert len(amino_acids) == 21, "There is not 21 amino-acids in the codon table"
 
 aa_table = {}
 for codon in codons:
@@ -77,7 +48,7 @@ for codon in codons:
     if aa not in aa_table:
         aa_table[aa] = []
     aa_table[aa].append(codons.index(codon))
-assert len(aa_table) == 21, "There is not 20 amino-acids in the aa table"
+assert len(aa_table) == 21, "There is not 21 amino-acids in the aa table"
 
 
 codon_preference = False
@@ -101,7 +72,6 @@ for aa, codon_list in aa_table.items():
 fitness_codons = nbr_weak*nbr_weak
 
 points = 50
-
 mut_bias_obs = np.zeros(points)
 gamma_ss = np.zeros(points)
 gamma_sw = np.zeros(points)
@@ -114,43 +84,10 @@ for mut_bias_id, mut_bias in enumerate(mut_bias_range):
                                 [mut_bias, 0, 1, mut_bias],
                                 [mut_bias, 1, 0, mut_bias],
                                 [mut_bias, 1, 1, 0]])
-    mutation_matrix -= np.diagflat(np.sum(mutation_matrix, axis=1))
-    assert abs(np.sum(mutation_matrix)) < 1e-10, "The mutation matrix don't have the rows summing to 0"
-
-    codon_sub_matrix = np.zeros((len(codons), len(codons)))
-    codon_mut_matrix = np.zeros((len(codons), len(codons)))
-
-    for c_origin_id, codon_origin in enumerate(codons):
-        for c_target_id, codon_target in enumerate(codons):
-            assert len(codon_origin) == len(codon_target)
-            mutations = [(n_from, n_to) for n_from, n_to in zip(codon_origin, codon_target) if n_from != n_to]
-            if len(mutations) == 1:
-                nuc_origin, nuc_target = mutations[0]
-                n_origin_id = nucleotides.find(nuc_origin)
-                n_target_id = nucleotides.find(nuc_target)
-                codon_mut_matrix[c_origin_id][c_target_id] = mutation_matrix[n_origin_id][n_target_id]
-                
-                sel_coef = fitness_codons[c_target_id] - fitness_codons[c_origin_id]
-                if abs(sel_coef) < 1e-10:
-                    p_fix = 1
-                else:
-                    p_fix = sel_coef / (1. - np.exp(-sel_coef))
-
-                codon_sub_matrix[c_origin_id][c_target_id] = mutation_matrix[n_origin_id][n_target_id] * p_fix
-
-    codon_sub_matrix -= np.diagflat(np.sum(codon_sub_matrix, axis=1))
-    assert abs(np.sum(codon_sub_matrix)) < 1e-10, "The codon matrix don't have the rows summing to 0"
-
-    codon_mut_matrix -= np.diagflat(np.sum(codon_mut_matrix, axis=1))
-    assert abs(np.sum(codon_mut_matrix)) < 1e-10, "The codon matrix don't have the rows summing to 0"
 
     codon_frequencies = np.power(mut_bias, nbr_weak)
     codon_frequencies *= np.exp(fitness_codons)
     codon_frequencies /= np.sum(codon_frequencies)
-
-    codon_sub_frequencies = equilibrium_frequencies(codon_sub_matrix)
-    if isinstance(codon_sub_frequencies, np.ndarray):
-        assert abs_sum(codon_sub_frequencies - codon_frequencies) < 1e-8, "The codon frequencies do not match"
 
     mut_bias_obs[mut_bias_id] = sum(codon_frequencies * z_ws) / sum(codon_frequencies * z_sw)
 
@@ -161,8 +98,15 @@ for mut_bias_id, mut_bias in enumerate(mut_bias_range):
             mutations = [(n_from, n_to) for n_from, n_to in zip(codon_origin, codon_target) if n_from != n_to]
             if len(mutations) == 1:
                 nuc_origin, nuc_target = mutations[0]
-                d_partiel = codon_frequencies[c_origin_id] * codon_sub_matrix[c_origin_id][c_target_id]
-                d0_partiel = codon_frequencies[c_origin_id] * codon_mut_matrix[c_origin_id][c_target_id]
+                n_origin_id = nucleotides.find(nuc_origin)
+                n_target_id = nucleotides.find(nuc_target)
+                sel_coef = fitness_codons[c_target_id] - fitness_codons[c_origin_id]
+                if abs(sel_coef) < 1e-10:
+                    p_fix = 1
+                else:
+                    p_fix = sel_coef / (1. - np.exp(-sel_coef))
+                d_partiel = codon_frequencies[c_origin_id] * mutation_matrix[n_origin_id][n_target_id] * p_fix
+                d0_partiel = codon_frequencies[c_origin_id] * mutation_matrix[n_origin_id][n_target_id]
                 if nuc_origin == "A" or nuc_origin == "T":
                     if nuc_target == "A" or nuc_target == "T":
                         dww += d_partiel
